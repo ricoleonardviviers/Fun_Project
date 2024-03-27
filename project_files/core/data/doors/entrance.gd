b27fbd99a64fbadd25cbddcee87ce901f556a8d5
Rico Viviers <rleonardviviers@gmail.com>
2024-03-27 16:21:45 +0200

    Fixed some isues with the entrance
    Fixed some isues with the entrance

--- project_files/core/data/doors/entrance.gd
+++ project_files/core/data/doors/entrance.gd
@@ -5,5 +5,5 @@ const player = preload("res://core/data/characters/player/player.tscn")
 
 func _ready() -> void:
 	var node = player.instantiate()
-	get_parent().add_child(node)
+	get_parent().add_child.call_deferred(node)
 	node.global_position = global_position
