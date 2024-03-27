f3cd89eb6359094d85c28414852f2635aa1b45d8
Rico Viviers <rleonardviviers@gmail.com>
2024-03-27 18:25:50 +0200

    Made Changes
    Modified the exit code
    Modified level a bit

--- project_files/core/data/doors/exit.gd
+++ project_files/core/data/doors/exit.gd
@@ -3,33 +3,17 @@ extends Node2D
 
 @export_file("*.tscn") var target_scene
 
-
-@onready var player : Player = get_node(^"../Player")
-
-
-func _ready() -> void:
-	player.key_lost.connect(Callable(self, "_on_key_lost"))
-	player.key_collected.connect(Callable(self, "_on_key_collected"))
-
+var entered = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player and body.has_key:
-		body.on_exit = true
-		body.exit = self
+	entered = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is Player:
-		body.on_exit = true
+	entered = false
 
 
-func _on_key_lost() -> void:
-	set_door_open(false)
-
-
-func _on_key_collected() -> void:
-	set_door_open(true)
-
-
-func set_door_open(value: bool) -> void:
-	pass
+func _process(delta: float) -> void:
+	if entered:
+		if  Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
